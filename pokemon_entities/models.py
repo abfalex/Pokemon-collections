2166b76a626f53aa63fc07346078a936a4e20469
abfalex <87718997+abfalex@users.noreply.github.com>
2024-06-10 18:24:50 +0300

    made fields optional

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -18,6 +18,9 @@ class Pokemon(models.Model):
     def __str__(self):
         return self.title
     
+    class Meta:
+        verbose_name = 'Покемон'
+    
 
 class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, related_name='entities', verbose_name='Покемон')
@@ -25,8 +28,11 @@ class PokemonEntity(models.Model):
     lon = models.FloatField(verbose_name='Долгота')
     appeared_at = models.DateTimeField(verbose_name='Время появления')
     disappeared_at = models.DateTimeField(verbose_name='Время исчезновения')
-    level = models.IntegerField(verbose_name='Уровень', default=0)
-    health = models.IntegerField(verbose_name='Здоровье', default=0)
-    attack = models.IntegerField(verbose_name='Атака', default=0)
-    protection = models.IntegerField(verbose_name='Уровень', default=0)
-    endurance = models.IntegerField(verbose_name='Выносливость', default=0)
+    level = models.IntegerField(verbose_name='Уровень', null=True, blank=True)
+    health = models.IntegerField(verbose_name='Здоровье', null=True, blank=True)
+    attack = models.IntegerField(verbose_name='Атака', null=True, blank=True)
+    protection = models.IntegerField(verbose_name='Уровень', null=True, blank=True)
+    endurance = models.IntegerField(verbose_name='Выносливость', null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Детали покемона'
